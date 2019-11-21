e1d1301e40f81eba4dc74c2410ce9ba5996a52ff
michael <mikelkarije@gmail.com>
2019-11-21 12:07:59 +0300

    created two methods for checking if both user and credentials exists

--- run.py
+++ run.py
@@ -23,3 +23,23 @@ def save_credential(Credential):
     function that save credential
     """
     Credential.save_credential()
+    
+def check_existing_user(name):
+    '''
+    check if user exists
+    '''
+    return User.user_exists(name)
+
+def find_credential(account_name):
+    '''
+    Function that finds credentials by account name
+    '''
+    # new_credential = Credential(acc, name, password)
+    return Credential.find_by_name(account_name)
+
+def existing_credentials(name):
+    '''
+    Functiion that checks if an account really exists
+    '''
+    Credential.credential_exists(name)
+    
