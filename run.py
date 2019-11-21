a388461165f85a25e204f1d8dcd08958815eb458
michael <mikelkarije@gmail.com>
2019-11-21 12:21:12 +0300

    for each of the provided short codes i gave them functions and methods to make the application run smoothly

--- run.py
+++ run.py
@@ -86,4 +86,116 @@ def main():
                     print('3:Delete Credentials')
                     print('4:Search Credentials')
                     print('5:Leave')
-                    
+                    selected = input()
+                    if selected == 'a':
+                        while True:
+                            print('Do you want to proceed? y/n')
+                            decision = input().lower()
+                            if decision == 'y':
+                                print("Input Account name")
+                                accname = input()
+                                print("Input username")
+                                uname = input()
+                                print("Input a password")
+                                print(
+                                    "Do you want a computer generated password ? use 'gen' or 'new' to create your own ")
+                                word = input().lower()
+                                if word == 'gen':
+                                    accpass = random.randint(0, 1000)
+                                    print(
+                                        f"Account:{accname} Username: {uname} Password:{accpass}")
+                                    print('\n')
+                                elif word == 'new':
+                                    print("Create password")
+                                    accpass = input()
+                                    print(
+                                        f"Account:{accname} Password:{accpass}")
+                                else:
+                                    print("Invalid choice")
+                                save_credential(
+                                    create_credential(accname, uname, accpass))
+                            elif decision == 'n':
+                                break
+                            else:
+                                print(
+                                    "Wrong choice use  either y to continue or n to stop")
+                    elif selected == 'b':
+                        while True:
+                            print('view your credenttial below')
+                            if display_credential():
+                                for creds in display_credential():
+                                    print(
+                                        f"Account Name:{creds.account_name} Username: {creds.username} Password:{creds.account_password}")
+                            else:
+                                print('\n')
+                                print(
+                                    "You don't have any saved credential before \n")
+                            print("Do you want to go back yes/no")
+                            reverse = input().lower()
+                            if reverse == 'yes':
+                                break
+                            elif reverse == 'no':
+                                continue
+                            else:
+                                print("Please input the correct choice")
+                                continue
+                    elif selected == 'c':
+                        while True:
+                            print("search for account to delete credential")
+                            find = input()
+                            if existing_credentials(find):
+                                accfind = find_credential(find)
+                                print(
+                                    "Are you sure you want to delete your account yes/no")
+                                done = input().lower()
+                                if done == 'yes':
+                                    delete_credential(accfind)
+                                    print("Accont deleted successfully")
+                                    break
+                                elif done == "no":
+                                    continue
+                            else:
+                                print("Credentialz does not exist")
+                            
+                            
+                    elif selected == 'd':
+                        while True:
+                            print("Want to proceed y/n")
+                            proc = input().lower()
+                            if proc == 'y':
+                                print(
+                                    "Input the name of the account to view credential")
+                                account = input()
+                                if existing_credentials(account):
+                                    found = find_credential(account)
+                                    print(f"ACCOUNT NAME:{found.account_name}")
+                                    print(f"PASSWORD {found.account_password}")
+                                else:
+                                    print("The account does not exist")
+                            elif proc == 'n':
+                                break
+                            else:
+                                print("Input a valid choice")
+                    elif selected == 'e':
+                        print("Are you sure you want to exit this site yes/no")
+                        exit == input().lower()
+                        if exit == 'yes':
+                            print("logged out successfully")
+                            break
+                        else:
+                            if exit == 'no':
+                                continue
+                    else:
+                        print("options not selected")
+                        continue
+            else:
+                print("The user name does not exist")
+                print("Create Account ")
+                print('*'*40)
+                print('Select Username')
+                username = input()
+                print('\n')
+                print('Password \n')
+                password = input()
+                print("signed up successfully")
+        
