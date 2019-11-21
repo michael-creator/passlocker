4e6efe20f3263085678a49289a7e4ee31d8b718d
michael <mikelkarije@gmail.com>
2019-11-21 11:32:02 +0300

    inside the credential file i have added new method for saving credential details

--- credentials.py
+++ credentials.py
@@ -7,4 +7,9 @@ class Credential:
         self.username = username
         self.account_name = account_name
         self.account_password = account_password
-    
+    def save_credential(self):
+        """
+        save_credential method  saves credential into credential_list
+        """
+        Credential.credentials_list.append(self)
+
