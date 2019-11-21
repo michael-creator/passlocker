4609680b60e83ccf775ca75b8765222e70c13ff0
michael <mikelkarije@gmail.com>
2019-11-21 11:07:39 +0300

    creation of user file and user class to define user details

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Class that generate new instance of us user.
+    """
+    user_list = []
+    def ___init___(self, login_username, user_password):
+        '''
+        initiating the variables
+        '''
+        self.login_username = login_username
+        self.user_password = user_password
+    # 
