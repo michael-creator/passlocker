fbe54f622730f978b64f32075d10bede05cbe197
michael <mikelkarije@gmail.com>
2019-11-21 11:42:28 +0300

    init test case to test if the object is initialized properly

--- credential_test.py
+++ credential_test.py
@@ -12,6 +12,12 @@ class TestUser(unittest.TestCase):
         """
         # self.new_user = User("mickey", "2019")
         self.new_credential = Credential("mickey", "facebook", "password")
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        self.assertEqual(self.new_credential.account_name, "facebook")
+        self.assertEqual(self.new_credential.account_password, "password")
    
 if __name__ == '__main__':
     unittest.main()
