aaddadc6b1f4eac4bda2b8dd95da580109b7c6be
michael <mikelkarije@gmail.com>
2019-11-21 11:30:19 +0300

    creationof credential file and defined credential details inside

---
+++ credentials.py
@@ -0,0 +1,10 @@
+class Credential:
+    """
+    class that generate new instances of credentials
+    """
+    credentials_list = []
+    def __init__(self, username, account_name, account_password):
+        self.username = username
+        self.account_name = account_name
+        self.account_password = account_password
+    
